2af4a8a829f8da5d6c206f17079fc9a1093c13ea
AWuhrmann <arthur.wuhrmann@epfl.ch>
2024-09-25 13:38:15 +0000

    changed git_sync.sh, added logs

--- git_sync.sh
+++ git_sync.sh
@@ -3,6 +3,19 @@
 # Path to your Git repository
 REPO_PATH="./vault/"
 
+LOG_FILE="./logs/git_sync.log"
+
+# Ensure the log directory exists, relative to the script's directory
+SCRIPT_DIR=$(dirname "$0") # Get the directory where the script is located
+LOG_DIR="$SCRIPT_DIR/logs" # Set the log directory relative to the script's directory
+
+# Create the logs directory if it doesn't exist
+if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
+fi
+
+echo $(ls)
+
 # Change to the repository directory
 cd $REPO_PATH || exit
 
@@ -16,10 +29,10 @@ git add .
 git commit -m "Automated commit at $TIMESTAMP"
 
 # Pull the latest changes from the remote repository
-git pull origin master
+git pull --rebase 
 
 # Push the changes to the remote repository
 git push --set-upstream origin master
 
 # Log the completion of the script execution
-echo "Git sync completed at $TIMESTAMP" >> /path/to/git_sync.log 2>&1
+echo "Git sync completed at $TIMESTAMP" >> ../logs/git_sync.log 1>&1

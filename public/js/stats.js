a3b47956da20652c30dc8b162f744050bb44e467
arthur <arthur.wuhrmann@epfl.ch>
2024-09-29 22:54:01 +0200

    added some reaction to the buttons

--- public/js/stats.js
+++ public/js/stats.js
@@ -3,8 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const statsButtons = document.getElementsByClassName('button-stats');
 
     for(let el of statsButtons){
-        el.addEventListener('click', function() {
-            console.log('hii');
-        });
+        el.addEventListener('click', function() {onClickStatsButton(el)});
     }
 });
+
+function onClickStatsButton(el){
+    console.log(el);
+    const counter = el.getElementsByClassName('count-text')[0];
+    console.log(counter);
+    counter.innerHTML = 'a';
+}

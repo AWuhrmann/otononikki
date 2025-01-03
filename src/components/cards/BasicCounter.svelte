6f847adec2cb74f1e22b2a30011831c4b81ef94b
arthur <arthur.wuhrmann@epfl.ch>
2025-01-03 22:50:08 +0100

    Added Basic counter element

---
+++ src/components/cards/BasicCounter.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+    import { CounterState } from "$lib/counter.svelte";
+
+    let {counter = new CounterState(0), name = 'My buttons\' name'} = $props();
+
+</script>
+
+<div class="button">
+
+    {name}
+
+    <button onclick={() => counter.increment()}>
+        Increment
+    </button>
+
+    <button onclick={() => counter.decrement()}>
+        Decrement
+    </button>
+
+    {counter.value}
+</div>
+
+<style>
+    .button {
+        height: 100px;
+        width: 300px;
+        background-color: lightgray;
+    }
+    
+</style>

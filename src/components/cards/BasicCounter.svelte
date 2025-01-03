ade30451beace7319fd058fe7e804dfd6f5c2327
arthur <arthur.wuhrmann@epfl.ch>
2025-01-03 23:32:15 +0100

    Improved style

--- src/components/cards/BasicCounter.svelte
+++ src/components/cards/BasicCounter.svelte
@@ -2,30 +2,72 @@
 
     import { CounterState } from "$lib/counter.svelte";
 
+    import { Plus, Minus } from 'lucide-svelte'
+
     let {counter = new CounterState(0), name = 'My buttons\' name'} = $props();
 
 </script>
 
-<div class="button">
+<div class="card">
 
-    {name}
+    <div class="info">
+        
+        <p class="font-bold">
+            {name}
+        </p>
+        
+        <p class="">{counter.value}</p>
 
-    <button onclick={() => counter.increment()}>
-        Increment
-    </button>
+    </div>
 
-    <button onclick={() => counter.decrement()}>
-        Decrement
-    </button>
 
-    {counter.value}
+    <div class="controls">
+
+        <button class="button" onclick={() => counter.increment()}>
+            <Plus/>
+        </button>
+        
+        <button class="button" onclick={() => counter.decrement()}>
+            <Minus/>
+        </button>
+    </div>
+
 </div>
 
 <style>
-    .button {
+    .card {
+        border-radius: 10px;
         height: 100px;
-        width: 300px;
-        background-color: lightgray;
+        width: 500px;
+        background-color: white;
+
+        display:flex;
+        justify-content: space-between;
     }
+
+    .info {
+        margin: auto 0;
+        padding-left: 10px;
+
+        font-family: Inter;
+        font-size: 20px;
+    }
+
+    .controls {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+
+        padding-right: 10px;
+    }
+
+    .button {
+        background-color: white;
+        box-shadow: none;
+        border: 0;
+    }
+
+    
+
     
 </style>

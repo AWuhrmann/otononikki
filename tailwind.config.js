a66f599fe6322ce0c36fb076cdee7f5efb5d2256
arthur <arthur.wuhrmann@epfl.ch>
2025-01-04 23:13:03 +0100

    added fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui',],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['SFMono-Regular'],
+      'courier': ['Courier'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    },
   },
   plugins: [],
 }

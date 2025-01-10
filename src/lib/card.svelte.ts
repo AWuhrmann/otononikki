06c105c7e5552c6052c418bdb7c948f22cb0e2b6
arthur <arthur.wuhrmann@epfl.ch>
2025-01-10 23:48:39 +0100

    Just brainstorming for today.

--- src/lib/card.svelte.ts
+++ src/lib/card.svelte.ts
@@ -1,6 +1,19 @@
 
 export type CardSettings = Record<string, string | number | boolean>;
 
+// List of interesting setting to have
+// Starting value, Min, max values
+// When to save (daily, each change)
+// auto Reset ? 
+// Goal settings ? 
+// increment (decrement) only
+// Average per day / all values
+// Show every x days ?
+// display of the card (for counter, etc.)
+// "Progress Stories" letting you attach notes to significant changes
+// Unit
+// Display size (small, etc.)
+
 interface CardValue {
   value: number;
   date: Date;

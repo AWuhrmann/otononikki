b5691f74dd792c4e6b243d37399eb62fffad2d3a
arthur <arthur.wuhrmann@epfl.ch>
2025-01-12 01:08:21 +0100

    improved stat page layout

--- src/routes/users/[name]/+page.svelte
+++ src/routes/users/[name]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { page } from "$app/state"
-    import Card from "$components/cards/Card.svelte"
+  import Card from "$components/cards/Card.svelte"
 
-  let { data } = $props();
+  let { data } = $props()
 </script>
 
 <div class="layout">
   {#if page.data.session && page.data.session.user && page.data.session.user.role === "admin"}
-    <h1>{data.username}</h1>
-    <div class="cards w-full flex flex-wrap justify-center gap-4">
+    <div class="cards w-full flex-col flex flex-wrap items-center gap-4">
+      <h1 class="text-4xl font-bold">{data.username}</h1>
       {#each data.cards as card}
         <div class="card">
           <Card {card} />

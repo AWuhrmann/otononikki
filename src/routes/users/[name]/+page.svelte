b1cf2de94686ef608c310aaa317f1348cf03054d
arthur <arthur.wuhrmann@epfl.ch>
2025-02-03 22:27:57 +0100

    idk what I did, let's say improve my understanding

--- src/routes/users/[name]/+page.svelte
+++ src/routes/users/[name]/+page.svelte
@@ -3,31 +3,30 @@
   import Card from "$components/cards/Card.svelte"
   import CardSettings from "$components/cards/CardSettings.svelte"
   import { getCards, load } from "$lib/card.svelte.js"
+  import { createCounter } from "$lib/counter_test.svelte.ts"
   import { onMount } from "svelte"
   
   let loading = $state(true)
   
+  const counter = createCounter();
+  const counter_2 = createCounter();
+
   let { data } = $props();
 
-  let cards = getCards();
+  let cards = $state([]);
 
   onMount(async () => {
     await load(data.username)
     loading = false
   })
 
-
-  const menuItems = [
-    {
-      label: "Option 1",
-      action: () => console.log("Option 1 clicked"),
-    },
-    {
-      label: "Option 2",
-      action: () => console.log("Option 2 clicked"),
-    },
-  ]
 </script>
+
+
+<button onclick={counter.increment}>
+	clicks: {counter.count}
+</button>
+<p>{counter_2.count}</p>
 
 <button onclick={() => {console.log(cards)}}>print cards</button>
 
@@ -36,7 +35,7 @@
       <div class="cards w-full flex-col flex flex-wrap items-center gap-4">
         <div class="flex justify-between w-[500px] mt-5">
           <h1 class="text-4xl font-bold">{data.username}</h1>
-          <CardSettings />
+            <!-- <CardSettings /> -->
         </div>
   
         {#if loading}

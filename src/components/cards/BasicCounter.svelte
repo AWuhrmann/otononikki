befebaa4a00235e9def9c632892b0bd6cd5c2c12
arthur <arthur.wuhrmann@epfl.ch>
2025-02-08 14:42:04 +0100

    fixed component

--- src/components/cards/BasicCounter.svelte
+++ src/components/cards/BasicCounter.svelte
@@ -8,6 +8,7 @@
   import CardSettings2 from "./CardSettings2.svelte"
   import Children from "$components/Children.svelte"
   import SuperParent from "$components/SuperParent.svelte"
+    import ActivityGrid from "$components/ActivityGrid.svelte"
 
   // I will try to implement floating UIs type shit :))
 
@@ -34,7 +35,6 @@
       return
     }
     value += 1
-    updateChart()
     saveCard(card, value)
   }
 
@@ -43,81 +43,7 @@
       return
     }
     value -= 1
-    updateChart()
     saveCard(card, value)
-  }
-
-  let colorClass = $derived(
-    "min_value" in card.settings && value <= card.settings.min_value
-      ? "text-gray-500 cursor-default"
-      : "text-black hover:opacity-80 transition-colors cursor-pointer",
-  )
-
-  $effect(() => {
-    updateChart()
-  })
-
-  function parseData(data, lastValue, n = 15) {
-    // First create the date map as before
-    const dateMap = {}
-
-    // Process input data
-    for (const item of data) {
-      const value =
-        typeof item.value === "string" ? parseFloat(item.value) : item.value
-      const date = new Date(item.timestamp).toISOString().split("T")[0] // YYYY-MM-DD
-      if (!dateMap[date] || item.timestamp > dateMap[date].timestamp) {
-        dateMap[date] = {
-          date,
-          value,
-          timestamp: item.timestamp,
-        }
-      }
-    }
-
-    // Find the date range
-    const dates = Object.keys(dateMap)
-    let startDate =
-      dates.length > 0
-        ? new Date(Math.min(...dates.map((d) => new Date(d))))
-        : new Date() // If no dates, use today
-    const today = new Date()
-    today.setHours(23, 59, 59, 999) // Set to end of day
-
-    // Fill in missing dates with zeros up to today
-    const allDates = []
-    const currentDate = new Date(startDate)
-
-    while (currentDate <= today) {
-      const dateStr = currentDate.toISOString().split("T")[0]
-      if (!dateMap[dateStr]) {
-        dateMap[dateStr] = {
-          date: dateStr,
-          value: 0,
-          timestamp: currentDate.getTime(),
-        }
-      }
-      allDates.push(dateStr)
-      currentDate.setDate(currentDate.getDate() + 1)
-    }
-
-    // Sort dates and create final array
-    const sortedData = allDates.map((date) => dateMap[date])
-
-    // Check if we need to modify the last value
-    if (sortedData.length > 0 && lastValue !== undefined) {
-      const lastEntry = sortedData[sortedData.length - 1]
-      if (lastEntry.value !== lastValue) {
-        lastEntry.value = lastValue
-      }
-    }
-
-    // Only keep the last n values
-    if (sortedData.length > n) {
-      sortedData.splice(0, sortedData.length - n)
-    }
-
-    return sortedData
   }
 
   function createSafeId(id: string) {
@@ -125,76 +51,11 @@
     return "chart-" + id
   }
 
-  function updateChart() {
-    // Get the last 10 values with their dates
-    const lastValue = value
-    const chartData = parseData(card.values, lastValue)
-    const chartId = createSafeId(card.id)
-
-    // Clear existing chart
-    d3.select("#" + chartId)
-      .selectAll("*")
-      .remove()
-
-    const margin = { top: 10, right: 10, bottom: 10, left: 10 }
-    const width = 200 - margin.left - margin.right
-    const height = 100 - margin.top - margin.bottom
-
-    // Create tooltip div
-    const tooltip = d3
-      .select("body")
-      .append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0)
-      .style("position", "absolute")
-      .style("background-color", "white")
-      .style("border", "1px solid #ddd")
-      .style("border-radius", "4px")
-      .style("padding", "5px")
-      .style("pointer-events", "none")
-
-    const svg = d3
-      .select("#" + chartId)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`)
-
-    const x = d3
-      .scaleBand()
-      .range([0, width])
-      .domain(d3.range(chartData.length))
-      .padding(0.1)
-
-    const y = d3
-      .scaleLinear()
-      .range([height, 0])
-      .domain([0, d3.max(chartData, (d) => d.value) || 0])
-
-    svg
-      .selectAll("rect")
-      .data(chartData)
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => x(i) ?? 0)
-      .attr("y", (d) => y(d.value))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => height - y(d.value))
-      .attr("rx", 4)
-      .attr("ry", 4)
-      .attr("fill", card_.settings["color"])
-      .on("mouseover", function (event, d) {
-        tooltip.transition().duration(200).style("opacity", 0.9)
-        tooltip
-          .html(d.date)
-          .style("left", event.pageX + 10 + "px")
-          .style("top", event.pageY - 28 + "px")
-      })
-      .on("mouseout", function (d) {
-        tooltip.transition().duration(500).style("opacity", 0)
-      })
-  }
+  let colorClass = $derived(
+    "min_value" in card.settings && value <= card.settings.min_value
+      ? "text-gray-500 cursor-default"
+      : "text-black hover:opacity-80 transition-colors cursor-pointer",
+  )
 
   $effect(() => {
     Object.entries(card_.settings).forEach(([key, value]) => {
@@ -229,7 +90,14 @@
       {#if "unit" in card.settings}{card.settings.unit}{/if}
     </p>
   </div>
-  <div class="w-[200px]" id={createSafeId(card.id)}></div>
+  <div class="w-[200px]" id={createSafeId(card.id)}>
+    <ActivityGrid 
+      values={card.values} 
+      monthYear={new Date().toISOString().slice(0, 7)} 
+      lastValue={getValue()} 
+      color={card.settings.color || '#000000'} 
+    />
+  </div>
   <div class="flex flex-col items-center pr-2 h-full">
     <SuperParent bind:card={card_} />
     <div class="flex-grow flex flex-col justify-center gap-0">
